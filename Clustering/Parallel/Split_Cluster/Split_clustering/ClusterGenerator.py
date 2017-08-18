39e0552589e32fd2c382faf594f098884b09ff32
arg-hya <akundu@lexmark.com>
2017-08-18 18:43:26 +0530

    1

--- Clustering/Parallel/Split_Cluster/Split_clustering/ClusterGenerator.py
+++ Clustering/Parallel/Split_Cluster/Split_clustering/ClusterGenerator.py
@@ -1,11 +1,11 @@
-from SOM_cluster_NOTPARALLEL  import SOM_clustering
+from SOM_cluster import SOM_clustering_parallel
 from kmeans import kmeans_clustering
 from CombineClusters import combineClusters  
 
 if __name__ == "__main__":
 
     print("SOM Clustering")
-    SOM_clustering();
+    SOM_clustering_parallel();
     print("kmeans Clustering")
     kmeans_clustering();
     print("Combining Clusters")
